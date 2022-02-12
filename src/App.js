d7ca515cc60b8216a05621463d5179e19036f961
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-02-12 23:27:17 +0530

    connect to wallet added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { ethers } from 'ethers';
 import Lock from './artifacts/contracts/Lock.sol/Lock.json';
 import './App.css';
 
+const lockAddress = process.env.REACT_APP_CONTRACT_LOCK_ADDRESS;
+
 function App() {
   const [userAccount, setUserAccount] = useState('');
-  const lock = process.env.REACT_APP_CONTRACT_LOCK_ADDRESS;
 
-  useEffect(() => {
-    console.log(lock);
-  })
+  async function requestAccount() {
+    await window.ethereum.request({method: 'eth_requestAccounts'})
+  }
 
   return (
     <div className="App">
-
+      <button onClick={requestAccount}>
+        Connect to wallet
+      </button>
     </div>
   );
 }
