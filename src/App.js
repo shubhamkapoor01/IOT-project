239d8a46b8438e3307a4fbeb729077b728fee05e
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-02-13 00:03:11 +0530

    temp qr scanner added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers';
 import Lock from './artifacts/contracts/Lock.sol/Lock.json';
+import QrReader from 'react-qr-reader';
 import './App.css';
 
 const lockAddress = process.env.REACT_APP_CONTRACT_LOCK_ADDRESS;
 
 function App() {
   const [userAccount, setUserAccount] = useState('');
+  const [QrScanResult, setQrScanResult] = useState('');
 
-  async function requestAccount() {
+  const requestAccount = async () => {
     await window.ethereum.request({method: 'eth_requestAccounts'})
   }
 
+  const handleError = (error) => {
+    console.log(error);
+  }
+
+  const handleScan = (result) => {
+    if (result) {
+      setQrScanResult(result);
+    }
+  }
+
   return (
-    <div className="App">
-      <button onClick={requestAccount}>
-        Connect to wallet
-      </button>
+    <div className="app">
+      <div className="qr-scanner">
+        <QrReader 
+          onError={ handleError }
+          onScan={ handleScan }
+        />
+        <h3>
+          {QrScanResult}
+        </h3>
+      </div>
     </div>
   );
 }
