1b90484aa3f0a13e844e7342e826a98f5696999a
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-02-13 22:47:07 +0530

    lock contract updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers';
-// import Lock from './artifacts/contracts/Lock.sol/Lock.json';
-import QrReader from 'react-qr-reader';
 import './App.css';
 
 const lockAddress = process.env.REACT_APP_CONTRACT_LOCK_ADDRESS;
 
 function App() {
-  const [userAccount, setUserAccount] = useState('');
-  const [QrScanResult, setQrScanResult] = useState('');
+  const [allowed, setAllowed] = useState([]);
+  const [owned, setOwned] = useState([]);
+
+  useEffect(() => {
+    getAllowed();
+    getOwned();
+  }, [])
+
+  const getAllowed = async () => {
+    const [account] = await window.ethereum.request({method: 'eth_requestAccounts'});
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(lockAddress, Lock.abi, provider);
+    const imAllowedTo = await contract.
+  }
+
+  const getOwned = async () => {
+
+  }
 
   const requestAccount = async () => {
     await window.ethereum.request({method: 'eth_requestAccounts'})
   }
 
-  const handleError = (error) => {
-    console.log(error);
-  }
-
-  const handleScan = (result) => {
-    if (result) {
-      setQrScanResult(result);
-    }
-  }
-
   return (
     <div className="app">
-      <div className="qr-scanner">
-        <QrReader 
-          onError={ handleError }
-          onScan={ handleScan }
-        />
-        <h3>
-          {QrScanResult}
-        </h3>
-      </div>
+
     </div>
   );
 }
